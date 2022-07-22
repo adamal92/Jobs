b0e850d261b522bc8c48bcee4a172feb4cdbfe42
adamal92 <livneadam@gmail.com>
2022-07-22 16:23:22 +0300

    added material list & fetches

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ITask } from './app.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'taskify';
+  word: any;
+  tasksList: ITask[] = [];
+
+  constructor() {  }
+
+  async getAllTasks() {
+    const res = await fetch("http://localhost:3000/", {
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
+    });
+    this.word = await res.text();
+    this.tasksList.push(this.word);
+    console.log(this.word)
+  }
+
+  async addTask() {
+    const res = await fetch("http://localhost:3000/", {
+      method: "POST",
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({
+        task_id: 2,
+        task_title: "second task",
+        task_describtion: "not so important",
+        task_status: 1
+      })
+    });
+    this.word = await res.text();
+    console.log(this.word)
+  } 
 }
---
+++ src/app/app.model.ts
@@ -0,0 +1,6 @@
+export interface ITask {
+    task_id: number,
+    task_title: string,
+    task_describtion: string,
+    task_status: number
+}
